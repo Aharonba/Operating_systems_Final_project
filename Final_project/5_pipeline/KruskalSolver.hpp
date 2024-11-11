7d6e1727309a4aa4854c74efb80f91bcea7d1b23
Aharonba <146740959+Aharonba@users.noreply.github.com>
2024-11-11 19:16:15 +0000

    Delete Final_project directory

--- Final_project/5_pipeline/KruskalSolver.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "MSTSolver.hpp"
-#include "MSTResult.hpp"
-
-class KruskalSolver : public MSTSolver
-{
-public:
-    MSTResult computeMST(const std::vector<std::tuple<int, int, int, int>> &edges, int vertexCount) override;
-};
